88afc52c52ebbf294c64e40edb6eb12714468894
ce20480 <aaronvinod25@gmail.com>
2025-04-13 14:24:36 +0100

    Slight change 4.0

--- scripts/test.py
+++ scripts/test.py
@@ -38,7 +38,7 @@ def run_lilypad_focus_module(image_path: str):
     command = [
         "lilypad",
         "run",
-        "github.com/ce20480/lilypad-module-focus-measure:aba681f178139a7b33cfcdf3f09bd4fb104e270f",
+        "github.com/ce20480/lilypad-module-focus-measure:21dee6d3dc1c412bab992a5f275dab6f4f0881f9",
     ]
 
     print("🚀 Running Lilypad focus measure module...")
